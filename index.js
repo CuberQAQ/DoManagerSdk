da1dde57fe541d764bea770d6e589e867f3cf2d7
CuberQAQ <1851445251@qq.com>
2023-04-02 22:26:02 +0800

    update

--- index.js
+++ index.js
@@ -14,13 +14,81 @@ let config = jsonfile.readFileSync("./dmng_config.json", {
 
 class DoManagerMemberMonitor {
     jsonObj = null
-    async pull() {
-        this.jsonObj = await $.getJSON(config.url + "/member")
+    async pull(url) {
+        return this.jsonObj = await $.getJSON((url ?? config.url) + "/member")
     }
-    async push() {
-        request.post(config.url + "/member", {
-            json: this.jsonObj
+    async push(url, json) {
+        request.post((url ?? config.url) + "/member", {
+            json: json ?? this.jsonObj
         })
+    }
+    addScore(target, score, reason, json) {
+        if(!isNaN(Number(target))) target = Number(target) 
+        let jsonObj = json ?? this.jsonObj
+        if (!jsonObj) return null
+        let targetMember = null
+        if (typeof (target) == "string") {
+            for (let i = 0; i < jsonObj.members.length; i++) {
+                const member = jsonObj.members[i];
+                if (member.name == target) {targetMember = member; break}
+            }
+        }
+        else if (typeof (target) == "number") {
+            for (let i = 0; i < jsonObj.members.length; i++) {
+                const member = jsonObj.members[i];
+                if (member.id == target) {targetMember = member; break}
+            }
+        }
+        if(!targetMember) return null
+        let score_num = Number(score)
+        if(isNaN(score_num)) return null
+        jsonObj.log.push({
+            id: targetMember.id,
+            operation: "add",
+            reason,
+            utc: Date.now(),
+            score: score_num
+        })
+        this.jsonObj = jsonObj
+        jsonfile.writeFileSync("./dmng_member.json",jsonObj,  {
+            encoding: "utf8"
+        })
+        targetMember.todayPes += score_num
+        return targetMember
+    }
+    removeScore(target, score, reason, json) {
+        if(!isNaN(Number(target))) target = Number(target) 
+        let jsonObj = json ?? this.jsonObj
+        if (!jsonObj) return null
+        let targetMember = null
+        if (typeof (target) == "string") {
+            for (let i = 0; i < jsonObj.members.length; i++) {
+                const member = jsonObj.members[i];
+                if (member.name == target) {targetMember = member; break}
+            }
+        }
+        else if (typeof (target) == "number") {
+            for (let i = 0; i < jsonObj.members.length; i++) {
+                const member = jsonObj.members[i];
+                if (member.id == target) {targetMember = member; break}
+            }
+        }
+        if(!targetMember) return null
+        let score_num = Number(score)
+        if(isNaN(score_num)) return null
+        jsonObj.log.push({
+            id: targetMember.id,
+            operation: "remove",
+            reason,
+            utc: Date.now(),
+            score: score_num
+        })
+        this.jsonObj = jsonObj
+        jsonfile.writeFileSync("./dmng_member.json",jsonObj,  {
+            encoding: "utf8"
+        })
+        targetMember.todayPes -= score_num
+        return targetMember
     }
     getMember(nameOrId) {
         if (!this.jsonObj) return null
